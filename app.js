199da1ee57a3d16986558a3dd475975dbddabec1
lashewi <swisum@gmail.com>
2020-07-11 18:42:07 +0530

    add google oauth20

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
+
+const passport = require('passport')
 const connectDB = require('./config/db')
 
 dotenv.config({ path: './config/config.env'})
+
+require('./config/passport')(passport)
 
 connectDB();
 
@@ -15,12 +20,28 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
+//handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}))
 app.set('view engine', '.hbs')
+
+//sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    
+  }))
+
+//passport
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+
 
 const PORT = process.env.PORT || 3000
 
