8b812c852931ee14f2ba55a567eb280667f086a7
lashewi <swisum@gmail.com>
2020-07-11 18:01:04 +0530

    add login hbs

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
@@ -16,6 +17,8 @@ if(process.env.NODE_ENV === 'development'){
 
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}))
 app.set('view engine', '.hbs')
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/index'))
 
