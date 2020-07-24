50a1a6841f45efa5775ad2836325cf654d312b37
lashewi <swisum@gmail.com>
2020-07-24 15:59:29 +0530

    add auth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 
+//@desc Auth with Google
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
+// @desc Google Auth callback
 router.get(
     '/google/callback',
     passport.authenticate('google', { failureRedirect:'/' }),
@@ -11,5 +13,13 @@ router.get(
         res.redirect('/dashboard')
     } 
 )
+
+//@desc logout User
+router.get('/logout', (req,res)=>{
+    req.logOut()
+    res.redirect('/')
+})
+
+
 
 module.exports = router
